81d207649c082a8b40491f5f306177ce8d7317db
Bogdan Popa <bogdan@defn.io>
2019-11-10 09:56:55 +0200

    test: run marionette during tests

--- marionette-test/tests/marionette/integration/common.rkt
+++ marionette-test/tests/marionette/integration/common.rkt
@@ -1,10 +1,13 @@
 #lang racket/base
 
-(require rackunit/text-ui)
+(require marionette
+         rackunit/text-ui)
 
 (provide
  run-integration-tests)
 
 (define (run-integration-tests . args)
   (when (equal? (getenv "MARIONETTE_INTEGRATION_TESTS") "x")
-    (apply run-tests args)))
+    (call-with-marionette!
+     (lambda _
+       (apply run-tests args)))))
