53ff4c7706c8b2526da1b99038ba1eeadfb78e21
Oscar <oscar@pinebook>
2023-05-28 14:28:34 -0400

    Fixing in-hash error when a port is provided and user.js isnt

--- marionette-lib/support/user.js
+++ marionette-lib/support/user.js
@@ -2,6 +2,7 @@ user_pref("marionette.port", @(or port 2828));
 user_pref("browser.startup.homepage_override.mstone", "ignore");
 user_pref("datareporting.policy.firstRunURL", "");
 
-@in[(k v) (in-hash user.js)]{
-  user_pref(@~s[k], @~js[v]);
-}
+@when[(hash? user.js)
+      @in[(k v) (in-hash user.js)]{
+	  user_pref(@~s[k], @~js[v]);
+      }]
