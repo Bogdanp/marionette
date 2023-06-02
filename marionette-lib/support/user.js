2b12c9d7d379797eb1317cd41fecd05a3e4c793c
Bogdan Popa <bogdan@defn.io>
2023-06-02 12:01:57 +0300

    support: avoid @when

--- marionette-lib/support/user.js
+++ marionette-lib/support/user.js
@@ -2,7 +2,6 @@ user_pref("marionette.port", @(or port 2828));
 user_pref("browser.startup.homepage_override.mstone", "ignore");
 user_pref("datareporting.policy.firstRunURL", "");
 
-@when[(hash? user.js)
-      @in[(k v) (in-hash user.js)]{
-	  user_pref(@~s[k], @~js[v]);
-      }]
+@in[(k v) (in-hash (or user.js (hash)))]{
+  user_pref(@~s[k], @~js[v]);
+}
