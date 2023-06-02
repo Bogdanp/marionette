daac17634775dc516611de6ea1e4c43776d64405
Bogdan Popa <bogdan@defn.io>
2023-06-02 12:21:45 +0300

    build: bump version

--- marionette-lib/info.rkt
+++ marionette-lib/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "1.2")
+(define version "1.2.1")
 (define collection "marionette")
 (define deps '("base"
                "scribble-text-lib"))
